64495210c38d78d016feb96508d511efa8cb76be
kidsage <ruhyun92@gmail.com>
2022-10-20 12:14:53 +0900

    Fix : 변경, 유저 모델 부분 삭제
    10/20(목)
     - 유저 모델에 perm 부분 삭제
       추후 Meta class에서 다시 구현할 예정
    
    Feat	새로운 기능을 추가할 경우
    Fix	버그를 고친 경우
    Design	CSS 등 사용자 UI 디자인 변경
    !BREAKING CHANGE	커다란 API 변경의 경우
    !HOTFIX	급하게 치명적인 버그를 고쳐야하는 경우
    Style	코드 포맷 변경, 세미 콜론 누락, 코드 수정이 없는 경우
    Refactor	프로덕션 코드 리팩토링
    Comment	필요한 주석 추가 및 변경
    Docs	문서를 수정한 경우
    Test	테스트 추가, 테스트 리팩토링(프로덕션 코드 변경 X)
    Chore	빌드 태스트 업데이트, 패키지 매니저를 설정하는 경우(프로덕션 코드 변경 X)
    Rename	파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우
    Remove	파일을 삭제하는 작업만 수행한 경우

--- accounts/models.py
+++ accounts/models.py
@@ -61,16 +61,6 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.username
 
-    # 로그인 사용자의 특정 테이블의 crud 권한을 설정, perm table의 crud 권한이 들어간다.
-    # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
-    def has_perm(self, perm, obj=None):
-        return True
-
-    # 로그인 사용자의 특정 app에 접근 가능 여부를 설정, app_label에는 app 이름이 들어간다.
-    # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
-    def has_module_perms(self, app_label):
-        return True
-
     # admin 권한 설정
     @property
     def is_staff(self):
