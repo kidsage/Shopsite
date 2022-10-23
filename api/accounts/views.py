f44469ff7c5bbc7aa45868a31e513628ec7da85a
kidsage <ruhyun92@gmail.com>
2022-10-23 23:02:22 +0900

    Feat : user view 기본형 추가
    10/23(일)
     - userlist, address, profile view 추가

--- api/accounts/views.py
+++ api/accounts/views.py
@@ -8,9 +8,9 @@ from .serializers import *
 
 #Create your views here.
 class UserSignInGenericAPIView(generics.GenericAPIView):
-    queryset            = User.objects.all()
-    serializer_class    = UserSerializer
-    permission_classes  = [AllowAny,]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny,]
 
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -23,3 +23,21 @@ class UserSignInGenericAPIView(generics.GenericAPIView):
         }
 
         return Response(response, status=status.HTTP_200_OK)
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny,]
+
+
+class AddressView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = Profile
+    permission_classes = [IsAuthenticatedOrReadOnly]
