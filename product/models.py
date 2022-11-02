84042a2db81e93e9952013333251b441dd1b9f36
kidsage <ruhyun92@gmail.com>
2022-11-02 13:12:15 +0900

    Feat: post app 추가
    11/2(수)
     - post app 추가 및 model 초기 세팅

--- product/models.py
+++ product/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=256, verbose_name='상품명')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     price = models.PositiveIntegerField(verbose_name='상품가격')
     description = models.TextField(verbose_name='상품설명')
     stock = models.IntegerField(verbose_name='재고')
@@ -13,3 +15,10 @@ class Product(models.Model):
 
     class Meta: 
     	db_table = 'product'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'categories'
