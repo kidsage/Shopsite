f284b6e9dacecf73d6263245a560b83b58d42489
kidsage <ruhyun92@gmail.com>
2022-11-04 17:25:58 +0900

    Feat: post view, serializer 초기 생성

---
+++ api/post/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from post.models import *
+from .serializers import *
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .accounts.views import *
+from .accounts.views import UserViewSet
+from .post.views import PostViewSet
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'user', UserViewSet)
+router.register(r'post', PostViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
