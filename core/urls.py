2cc911f5b0861b8a000d6ff90fab20ef78f8ae1e
kidsage <ruhyun92@gmail.com>
2022-11-13 22:52:51 +0900

    Test: jwt test

--- core/urls.py
+++ core/urls.py
@@ -23,9 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view, name='token_refresh'), # get a new token before the old expires.
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # jwt token test
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view, name='token_refresh'), # get a new token before the old expires.
+    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
