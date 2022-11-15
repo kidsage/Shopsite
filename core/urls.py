6088f77fd15152678695071a0ee7a5ef82a2fced
kidsage <ruhyun92@gmail.com>
2022-11-15 13:44:40 +0900

    Test: simple-jwt test 진행 중

--- core/urls.py
+++ core/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     # jwt token test
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view, name='token_refresh'), # get a new token before the old expires.
-    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
