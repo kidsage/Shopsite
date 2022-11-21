5e4750e92c674b62a59e87184b84c7873fb4aec8
kidsage <ruhyun92@gmail.com>
2022-11-21 23:18:14 +0900

    Test: order app 테스트

--- api/order/serializers.py
+++ api/order/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
 from order.models import Order
+# from ..accounts.serializers import UserSerializer
+from ..product.serializers import ProductSerializer
+from product.models import Product
 
 class OrderSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['user', 'product', 'quantity']
+
+    # 'OrderSerializer' object has no attribute 'product' 에러 해결 중
+    def create(self, validated_data):
+
+        order = Order.objects.create(
+            user = validated_data['user'],
+            product = validated_data['product'],
+            quantity = validated_data['quantity'],
+        )
+
+        p_update = Product.objects.update(
+            pk = self.product.pk,
+            stock = self.product['stock'] - validated_data['quantity']
+        )
+
+        return order
