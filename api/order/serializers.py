e8859b9c960a8ca76ccb2f97239adb61629a2ea8
kidsage <ruhyun92@gmail.com>
2022-11-27 10:54:37 +0900

    Feat: order serializer 조건 추가

--- api/order/serializers.py
+++ api/order/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from order.models import Order
 # from ..accounts.serializers import UserSerializer
 from ..product.serializers import ProductSerializer
@@ -22,8 +23,12 @@ class OrderSerializer(serializers.ModelSerializer):
         # product stock update
         # 일단 name으로 맞춰주긴 했는데, 뭔가 2번에 걸쳐 돌아가는 느낌이기는 하다.
         product_data = validated_data.pop('product')
-        p_update = Product.objects.filter(name=product_data.name).update(
-            stock = product_data.stock - validated_data['quantity']
-        )
+        if product_data.stock != 0:
+            p_update = Product.objects.filter(name=product_data.name).update(
+                stock = product_data.stock - validated_data['quantity']
+            )
+        else:
+
+            raise ValidationError('남은 수량이 없습니다.')
 
         return order
