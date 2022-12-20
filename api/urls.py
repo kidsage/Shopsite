7c3a55a7d1da9b2aa1eb5b4854f0b68880c8401c
kidsage <ruhyun92@gmail.com>
2022-12-20 22:14:46 +0900

    Feat: 장바구니 url 기본 틀 작성

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from rest_framework import routers
 from .accounts.views import UserViewSet, AddressViewSet
 from .post.views import PostViewSet, CommentViewSet
-from .order.views import OrderViewSet
+from .order.views import OrderViewSet, PaymentComplete, CartExistCheckAPI, OrderListAPI, CartListAPI
 from .product.views import ProductViewSet
+from django.views.generic import TemplateView
+
 
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
@@ -14,5 +16,15 @@ router.register(r'order', OrderViewSet)
 router.register(r'product', ProductViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('order/main', TemplateView.as_view(template_name='order/order.html'),
+         name='order-list'),
+    path('cart/', TemplateView.as_view(template_name='order/cart.html'),
+         name='cart'),
+    path('cart-api/', CartListAPI.as_view(), name='cart-api'),
+    path('order-api/', OrderListAPI.as_view(), name='order-api'),
+    path('exist-api/', CartExistCheckAPI.as_view(), name='exist-api'),
+    path('payments-complete/', PaymentComplete.as_view(),
+         name='payments-complete'),
 ]
+    
